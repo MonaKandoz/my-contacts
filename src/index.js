e6af7b92cc57cbdee00dc4acb3cf9bb98076a0ca
Mona Ramadan <mona.ramadan@webgenstudio.com>
2023-09-05 22:39:04 +0200

    added Contact component and error-page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import Root from "./routes/root";
+import Contact from "./routes/contact";
+import ErrorPage from "./error-page";
 
 import './index.css';
 
@@ -10,6 +13,11 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
